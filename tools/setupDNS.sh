7593654bdcccd05100fbb4274d83fd625e47b349
Ruud Lambregts <ruud.lambregts@springdigital.nl>
2019-04-09 09:44:16 +0200

    setup-fixes - Fixed issues in dns setup and added backward compat with yad domains

--- tools/setupDNS.sh
+++ tools/setupDNS.sh
@@ -1,29 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [ "$(uname)" == "linux-gnu" ]; then
-    echo "\n\nRunning on Linux "
-fi
+if [[ "$(uname)" == "Darwin" ]]; then
+    # Check if dnsmasq is already installed.
+    if [[ -n "$(command -v dnsmasq)" ]]; then
+        # The package is installed we do nothing
+        echo "Dnsmasq already installed, stopping daemon and adding config lines"
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "\n\nRunning on OSX, checking if dependencies are met."
-
-    if brew ls --versions dnsmasq; then
-      # The package is installed we do nothing
-      echo "\n\nYes DNSMASQ installed."
-
-      # Stop DNSMASQ service to make sure we can continue with the steps below
-      sudo launchctl stop homebrew.mxcl.dnsmasq
+        # Stop DNSMASQ service to make sure we can continue with the steps below
+        sudo launchctl stop homebrew.mxcl.dnsmasq
     else
-      # We know nothing Jon Snow (so we install dnsmasq)
+        # We know nothing Jon Snow (so we install dnsmasq)
         source brew.sh
     fi
 
-    echo 'address=/.sprd/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.conf
+    # Add .yad and .sprd to the config
+    echo 'address=/.yad/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.conf
+    echo 'address=/.sprd/127.0.0.1' >> $(brew --prefix)/etc/dnsmasq.conf
 
-    sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons
-    sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+    if ! [[ -f '/Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist' ]]; then
+        # Copy the daemon configuration file into place.
+        sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons
 
-    sudo mkdir -v /etc/resolver
+        # Start Dnsmasq automatically.
+        sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+    fi
+
+    if ! [[ -d '/etc/resolver' ]]; then
+        sudo mkdir -v /etc/resolver
+    fi
+
+    sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/yad'
     sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/sprd'
 
     # Make sure DNSMASQ is started
