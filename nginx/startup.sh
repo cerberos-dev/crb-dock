86bf21f3b32496f857a69fba1c6c75ce6a211087
Ruud Lambregts (Cerberos) <ruud.lambregts@cerberos.dev>
2021-03-15 18:12:13 +0100

    Added readability to commands

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -2,8 +2,17 @@
 
 if [ ! -f /etc/nginx/ssl/crb-dock.crt ]; then
     openssl genrsa -out "/etc/nginx/ssl/crb-dock.key" 2048
-    openssl req -new -key "/etc/nginx/ssl/crb-dock.key" -out "/etc/nginx/ssl/crb-dock.csr" -subj "/CN=CRB-Dock/O=CRB-Dock/C=NL" -addext "extendedKeyUsage=serverAuth"
-    openssl x509 -req -days 365 -in "/etc/nginx/ssl/crb-dock.csr" -signkey "/etc/nginx/ssl/crb-dock.key" -out "/etc/nginx/ssl/crb-dock.crt"
+
+    openssl req -new \
+      -key "/etc/nginx/ssl/crb-dock.key" \
+      -out "/etc/nginx/ssl/crb-dock.csr" \
+      -subj "/CN=CRB-Dock/O=CRB-Dock/C=NL" \
+      -addext "extendedKeyUsage=serverAuth"
+
+    openssl x509 -req -days 365 \
+      -in "/etc/nginx/ssl/crb-dock.csr" \
+      -signkey "/etc/nginx/ssl/crb-dock.key" \
+      -out "/etc/nginx/ssl/crb-dock.crt"
 
     chmod 644 /etc/nginx/ssl/crb-dock.key
 fi
